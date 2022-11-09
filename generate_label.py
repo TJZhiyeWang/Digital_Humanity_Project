1458976ffd7b1092b89e80fa076b2deaf1f5e237
Zhiye Wang <493878030@qq.com>
2022-11-09 22:05:34 +0100

    1931-1935 label check sample

--- generate_label.py
+++ generate_label.py
@@ -8,11 +8,11 @@ warnings.filterwarnings("ignore")
 PATH = "../inventory_sgi1931_r2022/SGI_1931.shp"
 seg = gpd.read_file(PATH)
 #EPSG:21781 x-y range
-x_min = 620004.99
-y_min = 170010.27
+x_min = 619741.59
+y_min = 170763.96
 
-x_max = 673440.04
-y_max = 132495.61
+x_max = 673471.64
+y_max = 132341.12
 x_gap = x_max - x_min
 y_gap = y_max - y_min
 print("x_gap: ",x_gap )
@@ -37,14 +37,14 @@ for i in tqdm(range(end_point, x_len)):
         x_list.append(i)
         y_list.append(j)
         points.append(Point(i * x_zoom + x_min, y_min + j * y_zoom))
-        if j % 5000 == 0 or j == y_len - 1:
+        if j % 3000 == 0 or j == y_len - 1:
             res = gpd.GeoDataFrame({'geometry': points, 'x':x_list, 'y':y_list}).sjoin(seg, how="inner", predicate='intersects')
             for _, (x,y) in enumerate(zip(res.x, res.y)):
                 labels[y,x] = True
             x_list = []
             y_list = []
             points = []
-    if i % 500 == 0:
-        np.save("./new_labels_small.npy", labels)
+    # if i % 500 == 0:
+    #     np.save("./new_labels_small.npy", labels)
 
-np.save("./new_labels_small.npy", labels)
+np.save("./new_labels_small_1931.npy", labels)
